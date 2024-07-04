c38da81c70cfeed4da882ec047e5e5406fe7b81e
quangd42 <quang.dang.n42@gmail.com>
2024-07-04 00:39:02 -0400

    ft: use db transaction to update Recipe

--- backend/handlers/handler_recipes.go
+++ backend/handlers/handler_recipes.go
@@ -134,26 +134,7 @@ func updateRecipeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	type step struct {
-		StepNo      int    `json:"step_no"`
-		Instruction string `json:"instruction"`
-	}
-	type parameters struct {
-		Name              string  `json:"name"`
-		ExternalURL       string  `json:"external_url"`
-		Servings          int     `json:"servings"`
-		Yield             *string `json:"yield"`
-		CookTimeInMinutes int     `json:"cook_time_in_minutes"`
-		Notes             *string `json:"notes"`
-		Ingredients       []struct {
-			ID       uuid.UUID `json:"id"`
-			Amount   string    `json:"amount"`
-			PrepNote *string   `json:"prep_note"`
-		} `json:"ingredients"`
-		Instructions []step `json:"instructions"`
-	}
-
-	params := &parameters{}
+	params := &database.UpdateAllRecipeDataParams{}
 	err = json.NewDecoder(r.Body).Decode(params)
 	if err != nil {
 		respondMalformedRequestError(w)
@@ -171,120 +152,20 @@ func updateRecipeHandler(w http.ResponseWriter, r *http.Request) {
 		respondError(w, http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
 		return
 	}
+	params.ID = targetRecipe.ID
 
-	// Update host Recipe
-	recipe, err := DB.UpdateRecipeByID(r.Context(), database.UpdateRecipeByIDParams{
-		ID:                targetRecipe.ID,
-		UpdatedAt:         time.Now().UTC(),
-		Name:              params.Name,
-		ExternalUrl:       params.ExternalURL,
-		Servings:          int32(params.Servings),
-		Yield:             params.Yield,
-		CookTimeInMinutes: int32(params.CookTimeInMinutes),
-		Notes:             params.Notes,
-	})
 	if err != nil {
 		respondInternalServerError(w)
 		return
 	}
 
-	// Update ingredients in Recipe
-	for _, i := range params.Ingredients {
-		ingreDBParams := database.UpdateIngredientInRecipeParams{
-			Amount:       i.Amount,
-			PrepNote:     i.PrepNote,
-			UpdatedAt:    time.Now().UTC(),
-			IngredientID: i.ID,
-			RecipeID:     recipeID,
-		}
-
-		err = DB.UpdateIngredientInRecipe(r.Context(), ingreDBParams)
-		if err != nil {
-			respondInternalServerError(w)
-			return
-		}
-	}
-
-	ingredients, err := DB.ListIngredientsByRecipeID(r.Context(), recipeID)
+	recipe, err := database.UpdateAllRecipeData(r.Context(), store, *params)
 	if err != nil {
-		respondInternalServerError(w)
+		respondMalformedRequestError(w)
 		return
 	}
 
-	// Update instructions in Recipe
-	// List instructions from db
-	dbInstructions, err := DB.ListInstructionsByRecipeID(r.Context(), recipe.ID)
-	if err != nil {
-		respondInternalServerError(w)
-		return
-	}
-
-	// Make map from db
-	dbInstructionsMap := make(map[int]database.Instruction, len(dbInstructions))
-	for _, dbi := range dbInstructions {
-		dbInstructionsMap[int(dbi.StepNo)] = dbi
-	}
-
-	var toAdd, toUpdate []step
-	for _, pi := range params.Instructions {
-		_, ok := dbInstructionsMap[pi.StepNo]
-		// If in param the step no is found in db, add it to the update list
-		// then delete it from the map
-		if ok {
-			toUpdate = append(toUpdate, pi)
-			delete(dbInstructionsMap, pi.StepNo)
-			// Else the param is new, add it to the add list
-		} else {
-			toAdd = append(toAdd, pi)
-		}
-	}
-
-	for _, pi := range toAdd {
-		err = DB.AddInstructionToRecipe(r.Context(), database.AddInstructionToRecipeParams{
-			CreatedAt:   time.Now().UTC(),
-			UpdatedAt:   time.Now().UTC(),
-			Instruction: pi.Instruction,
-			StepNo:      int32(pi.StepNo),
-			RecipeID:    recipe.ID,
-		})
-		if err != nil {
-			respondInternalServerError(w)
-			return
-		}
-	}
-
-	for _, pi := range toUpdate {
-		err = DB.UpdateInstructionByID(r.Context(), database.UpdateInstructionByIDParams{
-			UpdatedAt:   time.Now().UTC(),
-			Instruction: pi.Instruction,
-			StepNo:      int32(pi.StepNo),
-			RecipeID:    recipe.ID,
-		})
-		if err != nil {
-			respondUniqueValueError(w, err, "step_no must be unique for each step")
-			return
-		}
-	}
-
-	// The rest of the dbMap is not found in param, so delete them
-	for _, dbi := range dbInstructionsMap {
-		err = DB.DeleteInstructionByID(r.Context(), database.DeleteInstructionByIDParams{
-			StepNo:   dbi.StepNo,
-			RecipeID: dbi.RecipeID,
-		})
-		if err != nil {
-			respondInternalServerError(w)
-			return
-		}
-	}
-
-	instructions, err := DB.ListInstructionsByRecipeID(r.Context(), recipe.ID)
-	if err != nil {
-		respondInternalServerError(w)
-		return
-	}
-
-	respondJSON(w, http.StatusOK, createRecipeResponse(recipe, ingredients, instructions))
+	respondJSON(w, http.StatusOK, recipe)
 }
 
 func listRecipesHandler(w http.ResponseWriter, r *http.Request) {
