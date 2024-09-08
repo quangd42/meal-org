3643dece520d2486668807a53b7dc609ca4bd076
quangdn42 <quangdn42@gmail.com>
2024-09-08 12:02:35 +0700

    organize and comment

--- internal/views/recipes/forms/utils.go
+++ internal/views/recipes/forms/utils.go
@@ -1,5 +1,6 @@
 package forms
 
+// HACK: This is not applied to InputText. If change styling here, remember to change it in InputText as well.
 func isRequiredStyling(v bool) map[string]bool {
 	return map[string]bool{
 		"after:content-['_*']": v,
