eb7e3f4213b2d538db8022aaa273f28341ea3158
quangd42 <quangdn42@gmail.com>
2024-12-01 15:42:06 +0700

    refactor: move saveExternalImage to be a services private method

--- internal/handlers/handler_api_recipes.go
+++ internal/handlers/handler_api_recipes.go
@@ -21,7 +21,6 @@ type RecipeService interface {
 	ListRecipesByUserID(ctx context.Context, userID uuid.UUID, pgn models.RecipesPagination) ([]models.RecipeInList, error)
 	DeleteRecipeByID(ctx context.Context, recipeID uuid.UUID) error
 	ListRecipesWithCuisinesByUserID(ctx context.Context, userID uuid.UUID, pgn models.RecipesPagination) ([]models.RecipeInList, error)
-	SaveExternalImage(recipeID uuid.UUID, recipeURL *string)
 }
 
 // TODO: allow for uploading images
